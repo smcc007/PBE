bea6a791039ecb5e7531f3b4e14ee1c1aa6fe00f
smcc007 <stephen.d.mcc@gmail.com>
2017-07-10 10:06:54 -0400

    Convert 1st input string to all upper case to ensure characters are mapped to alphabet. Updated exception catching if character shift fails in any way.

--- PBE_caeser.py
+++ PBE_caeser.py
@@ -1,33 +1,28 @@
-#To Do:
-#1.) configure all code below the for loop into a function
-    #a.) pass in orgTxt and assert it is text
-    #b.) pass in shift_by and assert it is an integer
-    #c.) constrain the shift to wrap around and only use alphabetic characters.
-#2.) read up on main function in python...start creating a class
+import sys
 
-#Define alphabet of characters for mapping
+#Define alphabet of characters for mapping, can be extended as required.
 arr_Alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R','S','T', 'R', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
 
-#Declare local variables
-#To be replaced by vairables passed into the scrip
-#Or write this as a class.
-orgTxt = "BACZ"
+#Declare local variables from
+orgTxt = (sys.argv[1]).upper()
+shift_by = int(sys.argv[2]) #shifted by values
+
+print ("This is the orginal text:   " + orgTxt + "and it will be shifted by %d places" % shift_by)
+
 cipherTxt = ""
-shift_by = 2
 shifted_index = 0
 
 #iterate over the entire length of the input string
 for x in range(0, len(orgTxt)):
-    #print ("Original character: %s, after shift %s " % (input[x], chr(ord(input[x])+shift_by)))
-    #find key
-    #find character position in arr_Alpha
-    print orgTxt[x]
-    idx = arr_Alpha.index(orgTxt[x])
-
-    #shift by shift_by value with wrap around length of list
-    shifted_index = (idx + shift_by) % len(arr_Alpha)
-
-    cipherTxt += arr_Alpha[shifted_index]
+    try: #character shift
+        #find character position in arr_Alpha
+        idx = arr_Alpha.index(orgTxt[x])
+        #shift by shift_by value with wrap around length of list
+        shifted_index = (idx + shift_by) % len(arr_Alpha)
+        cipherTxt += arr_Alpha[shifted_index]
+    except: #if the caeser substitution fails in anyway, keep the orginal
+            #character
+        cipherTxt += orgTxt[x]
 
 print ("Original text:  %s" %orgTxt)
 print ("Output text:    %s" %cipherTxt)
